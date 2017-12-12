3ef7bd3c3075d5ce75a7016728d0b6aadbd9ff2c
ZBK <1498821723@qq.com>
2017-12-12 19:10:31 +0800

    first commit

--- app/src/main/java/com/live/communityskyeye/SceneActivity.java
+++ app/src/main/java/com/live/communityskyeye/SceneActivity.java
@@ -2,49 +2,44 @@ package com.live.communityskyeye;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.litepal.crud.DataSupport;
 import org.litepal.tablemanager.Connector;
 
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.List;
-
-import static android.R.id.list;
 
 public class  SceneActivity extends AppCompatActivity {
 
-
-
+    List<Place>  places = DataSupport.findAll(Place.class);
+    private ListView listview = null;
+   // private  List<Place> places = new ArrayList<>();
     int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scene);
        // System.out.println(DataSupport.count(Place.class));
-        Connector.getDatabase();
-        List<Place>  places = DataSupport.findAll(Place.class);
+       // initScene();
+        //Connector.getDatabase();
         String[] save_place= new String[places.size()];
-
+       // System.out.println(DataSupport.count(Place.class));
         for (Place place : places){
-            save_place[counter] = place.getPlace_name();
-            counter++;
+           save_place[counter] = place.getPlace_name();
+         counter++;
         }
-
-
-
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 SceneActivity.this, android.R.layout.simple_list_item_1,save_place);
-        ListView listview = (ListView) findViewById(R.id.secene_list) ;
+        listview = (ListView) findViewById(R.id.secene_list) ;
         listview.setAdapter(adapter);
 
         findViewById(R.id.add_scene).setOnClickListener(new View.OnClickListener() {
@@ -54,6 +49,17 @@ public class  SceneActivity extends AppCompatActivity {
 
             }
         });
+
+       listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Intent intent = new Intent();
+               intent.putExtra("sceneName", (String) listview.getItemAtPosition(position));
+               intent.setClass(SceneActivity.this,SubScene.class);
+              // System.out.println((String)listview.getItemAtPosition(position));
+               startActivity(intent);
+           }
+       });
 
 
 
@@ -68,6 +74,7 @@ public class  SceneActivity extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(SceneActivity.this);
                 builder.setMessage("确定删除?");
                 builder.setTitle("提示");
+                System.out.println(DataSupport.count(Place.class));
              //   System.out.println(DataSupport.count(Place.class));
                 System.out.println("position is"+position);
 
@@ -77,8 +84,9 @@ public class  SceneActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         System.out.println("position is"+position);
                         Connector.getDatabase();
-                        DataSupport.delete(Place.class,position);
-                        System.out.println(DataSupport.count(Place.class));
+                        DataSupport.delete(Place.class,position+1);
+                       // DataSupport.update(Place.class);
+                       // System.out.println(DataSupport.count(Place.class));
 
                         //adapter.notifyDataSetChanged();
                         Toast.makeText(getBaseContext(),"已删除",Toast.LENGTH_SHORT).show();
@@ -95,8 +103,8 @@ public class  SceneActivity extends AppCompatActivity {
 
                 builder.create().show();
                // startActivity(new Intent(SceneActivity.this,SceneActivity.class));
-
-                return false;
+                //长按与短按的区别
+                return true;
             }
         });
 
@@ -104,7 +112,17 @@ public class  SceneActivity extends AppCompatActivity {
 
     }
 
+    public void initScene(){
+        Connector.getDatabase();
+        List<Place> places = DataSupport.findAll(Place.class);
+        //List<Place> places = new ArrayList<>();
+        String[] save_place= new String[places.size()];
+        for (Place place : places){
+            save_place[counter] = place.getPlace_name();
+            counter++;
+        }
 
+    }
 
 
 
