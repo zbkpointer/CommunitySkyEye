19fd3a5257f74df108febd591589ec9e4589a98e
ZBK <1498821723@qq.com>
2017-12-25 11:37:19 +0800

    大疆产品连接

---
+++ app/src/main/java/com/live/communityskyeye/ConnectionActivity.java
@@ -0,0 +1,89 @@
+package com.live.communityskyeye;
+
+import android.*;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ConnectionActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = ConnectionActivity.class.getName();
+    private TextView mTextConnectionStatus;
+    private TextView mTextProduct;
+    private Button mBtnOpen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*
+         When the compile and target version is higher than 22,
+         please request the following permission at runtime to
+         ensure the SDK works well.
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.VIBRATE,
+                            android.Manifest.permission.INTERNET, android.Manifest.permission.ACCESS_WIFI_STATE,
+                            android.Manifest.permission.WAKE_LOCK, android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            android.Manifest.permission.ACCESS_NETWORK_STATE, android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            android.Manifest.permission.CHANGE_WIFI_STATE, android.Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.SYSTEM_ALERT_WINDOW,
+                            android.Manifest.permission.READ_PHONE_STATE,
+                    }
+                    , 1);
+        }
+        setContentView(R.layout.activity_connection);
+
+    }
+
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        Log.e(TAG, "onPause");
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        Log.e(TAG, "onStop");
+        super.onStop();
+    }
+    public void onReturn(View view){
+        Log.e(TAG, "onReturn");
+        this.finish();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+    }
+    private void initUI() {
+        mTextConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
+        mTextProduct = (TextView) findViewById(R.id.text_product_info);
+        mBtnOpen = (Button) findViewById(R.id.btn_open);
+        mBtnOpen.setOnClickListener(this);
+        mBtnOpen.setEnabled(false);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_open: {
+                break;
+            }
+            default:
+                break;
+        }
+
+
+    }
+}
